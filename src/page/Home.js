c9daf9880ae31c51ce2278a34679e0000053df95
katriosa <ee.chistyakova@gmail.com>
2023-09-03 21:42:22 +0200

    add animation to home page

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,19 +1,30 @@
+import React from "react";
 import classes from "./Home.module.css";
 import { Link } from "react-router-dom";
-import React from "react";
+import owl from "../assets/owl.json";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 const HomePage = () => {
   return (
     <>
-      <main>
-        <div className={classes["bg-image"]}></div>
-        <div className={classes.content}>
-          <h1>
-            Угадай <span>птицу</span> по голосу
-          </h1>
-          <Link to="/game">Начать</Link>
-        </div>
-      </main>
+      <div className={classes["bg-image"]}>
+        <main className={classes.content}>
+          <div className={classes.animationContainer}>
+            <Player
+              autoplay
+              loop
+              src={owl}
+              style={{ height: "150px", width: "150px" }}
+            ></Player>
+          </div>
+          <div className={classes.header}>
+            <h1>Угадай птицу по голосу</h1>
+          </div>
+          <div className={classes.startButton}>
+            <Link to="/game">Начать</Link>
+          </div>
+        </main>
+      </div>
       <footer className={classes.footer}>
         <p>
           Автор: <a href="https://github.com/katriosa">katriosa</a>
@@ -22,4 +33,5 @@ const HomePage = () => {
     </>
   );
 };
+
 export default HomePage;
